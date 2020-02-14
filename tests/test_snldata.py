85881047d4a7015df84aab6f4ffa2026db0d471e
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2020-02-14 16:53:40 +0100

    Create test_snldata.py

---
+++ tests/test_snldata.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest.mock import patch, MagicMock
+
+class snlDataTestCase(unittest.TestCase):
+    maxDiff = None
+    pass
+    
+if __name__ == "__main__":
+    unittest.main(module="test_black")
