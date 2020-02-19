f0252ac19d3a6129234d00378a312de7a60c55da
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2020-02-19 12:47:46 +0100

    Update test_snldata.py

--- tests/test_snldata.py
+++ tests/test_snldata.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python3
-
 import unittest
-from unittest.mock import patch, MagicMock
+import SnlData.snldata
 
-class snlDataTestCase(unittest.TestCase):
-    maxDiff = None
-    pass
-    
-if __name__ == "__main__":
-    unittest.main(module="test_black")
+class TestSnlData(unittest.TestCase):
+
+    def setUp(self):
+        self.service = snldata.SnlSession()
+        super().setUp()
+
+    def tearDown(self):
+        self.service.close()
+        
+    def testQuery(self):
+        self.service.search(query="aa-", best=True)
+        self.assertEqual( self.service.title, "aa-")
+        
+if __name__ == '__main__':
+    unittest.main()
