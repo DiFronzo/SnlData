7c0ad2390a6d0706bd99d4cc1bbe59cf67f112da
Andreas Lien <30557582+DiFronzo@users.noreply.github.com>
2020-02-22 14:05:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -67,6 +67,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Quality Assurance",
     ],
 )
